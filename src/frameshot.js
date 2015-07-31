5f3432c0c1166ec4968fcf1aac4256a7edb15968
Bingli Xu <binglixu@163.com>
2015-07-31 18:31:48 +0800

    qunit tdd setup

---
+++ src/frameshot.js
@@ -0,0 +1,23 @@
+/*!
+ * FrameShot - Web App to capture video frames
+ *
+ * Version:  1.0.1
+ * Released: 2012-05-04
+ * Source:   http://github.com/appendto/jquery-mockjax
+ * Docs:     http://enterprisejquery.com/2010/07/mock-your-ajax-requests-with-mockjax-for-rapid-development
+ * Plugin:   FrameShot
+ * Author:   Bingli Xu (http://jdsharp.com)
+ * License:  MIT,GPL
+ * 
+ * Copyright (c) 2010 appendTo LLC.
+ * Dual licensed under the MIT or GPL licenses.
+ * http://appendto.com/open-source-licenses
+ */
+ 
+(function($) {
+
+
+
+
+
+})(jQuery);
---
+++ test/test_frameshot.js
@@ -0,0 +1,24 @@
+/*
+ * FrameShot App Tests
+ *
+ */
+(function($) {
+
+module("video control test", {
+	setup: function() {
+		// state = true;
+		// ok(true);
+	},
+	teardown: function() {
+		// ok(true);
+	}
+});
+
+test("the video object exists", function() {
+	// expect(3);
+	ok(true);
+});
+
+
+
+})(jQuery);
